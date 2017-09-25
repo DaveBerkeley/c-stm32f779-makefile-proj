ab27db9fbe3e27b426bae6cb64a061ab1ee097f2
Bram Vlerick <bram.vlerick@barco.com>
2017-09-25 09:34:59 +0200

    Added UART using STM32Cube

--- Makefile
+++ Makefile
@@ -19,6 +19,15 @@ SOURCES_C += Drivers/CMSIS/Device/ST/STM32F7xx/Source/Templates/system_stm32f7xx
 SOURCES_C += Drivers/BSP/STM32F769I_EVAL/stm32f769i_eval.c
 SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_gpio.c
 SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_adc.c
+SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal.c
+SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_cortex.c
+SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart.c
+SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.c
+SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc_ex.c
+SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_pwr_ex.c
+SOURCES_C += Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_wwdg.c
+SOURCES_C += src/stm32f7xx_it.c
+SOURCES_C += src/stm32f7xx_hal_msp.c
 
 SOURCES = $(SOURCES_S) $(SOURCES_C)
 OBJS = $(SOURCES_S:.s=.o) $(SOURCES_C:.c=.o)
@@ -58,7 +67,7 @@ MCUFLAGS = -mcpu=cortex-m7 -mlittle-endian
 MCUFLAGS += -mfloat-abi=hard -mfpu=fpv5-sp-d16
 MCUFLAGS += -mthumb
 
-DEBUGFLAGS = -Og -g -gdwarf-2
+DEBUGFLAGS = -O0 -g -ggdb
 #DEBUGFLAGS = -O2
 
 #CFLAGS = -std=c11
@@ -69,7 +78,7 @@ CFLAGS_EXTRA += -Wl,--gc-sections -Wl,-Map=$(PROJECT).map
 
 CFLAGS += $(DEFINES) $(MCUFLAGS) $(DEBUG_FLAGS) $(CFLAGS_EXTRA) $(INCLUDES)
 
-LDFLAGS = -static $(MCUFLAGS)
+LDFLAGS = -static $(MCUFLAGS) $(DEBUG_FLAGS)
 LDFLAGS += -Wl,--start-group -lgcc -lm -lc -lg -Wl,--end-group
 
 # Enable Semihosting
--- src/main.c
+++ src/main.c
@@ -1,35 +1,127 @@
 #include "stm32f7xx_hal.h"
 #include "stm32f769i_eval.h"
 #include "stm32f769i_eval_io.h"
+#include "stm32f7xx_it.h"
 
+
+/* Definition for USARTx clock resources */
+#define USARTx                           USART1
+#define USARTx_CLK_ENABLE()              __USART1_CLK_ENABLE()
+#define USARTx_RX_GPIO_CLK_ENABLE()      __GPIOA_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()      __GPIOA_CLK_ENABLE()
+
+#define USARTx_FORCE_RESET()             __USART1_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __USART1_RELEASE_RESET()
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_9
+#define USARTx_TX_GPIO_PORT              GPIOA
+#define USARTx_TX_AF                     GPIO_AF7_USART1
+#define USARTx_RX_PIN                    GPIO_PIN_10
+#define USARTx_RX_GPIO_PORT              GPIOA
+#define USARTx_RX_AF                     GPIO_AF7_USART1
+
+
+UART_HandleTypeDef uart_handle;
 
 static void init_cpu_caches(void)
 {
-        SCB_EnableICache();
-        SCB_EnableDCache();
+	SCB_EnableICache();
+	SCB_EnableDCache();
+}
+
+static void SystemClock_Config(void)
+{
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	HAL_StatusTypeDef  ret = HAL_OK;
+
+	/* Enable Power Control clock */
+	__HAL_RCC_PWR_CLK_ENABLE();
+
+	/* The voltage scaling allows optimizing the power consumption when the device is
+	   clocked below the maximum system frequency, to update the voltage scaling value
+	   regarding system frequency refer to product datasheet.  */
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	/* Enable HSE Oscillator and activate PLL with HSE as source */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 25;
+	RCC_OscInitStruct.PLL.PLLN = 432;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 9;
+	RCC_OscInitStruct.PLL.PLLR = 7;
+
+	ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	if(ret != HAL_OK)
+	{
+		while(1) { ; }
+	}
+
+	/* Activate the OverDrive to reach the 216 MHz Frequency */
+	ret = HAL_PWREx_EnableOverDrive();
+	if(ret != HAL_OK)
+	{
+		while(1) { ; }
+	}
+
+	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+	ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7);
+	if(ret != HAL_OK)
+	{
+		while(1) { ; }
+	}
 }
 
 int main(void)
 {
-        int i = 0;
-        init_cpu_caches();
+	int i = 0;
+	uint8_t ch = 'z';
+	init_cpu_caches();
+	HAL_Init();
+	SystemClock_Config();
 
-        BSP_LED_Init(LED_GREEN);
-        BSP_LED_Init(LED_RED);
-        BSP_LED_Init(LED_BLUE);
-        BSP_LED_Init(LED_ORANGE);
+	BSP_LED_Init(LED_GREEN);
+	BSP_LED_Init(LED_RED);
+	BSP_LED_Init(LED_BLUE);
+	BSP_LED_Init(LED_ORANGE);
 
-        BSP_LED_Toggle(LED_GREEN);
-        BSP_LED_Toggle(LED_RED);
+	BSP_LED_Toggle(LED_GREEN);
+	BSP_LED_Toggle(LED_RED);
 
-        for(;;)
-        {
-                BSP_LED_Toggle(LED_GREEN);
-                BSP_LED_Toggle(LED_RED);
-                BSP_LED_Toggle(LED_ORANGE);
-                BSP_LED_Toggle(LED_BLUE);
-                for (int j = 0; j < 1000000; j++);
-                printf("Hello, world! -- %i\n", i++);
-        }
-        return 0;
+	uart_handle.Instance = USARTx;
+	uart_handle.Init.BaudRate = 9600;
+	uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
+	uart_handle.Init.StopBits   = UART_STOPBITS_1;
+	uart_handle.Init.Parity     = UART_PARITY_NONE;
+	uart_handle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+	uart_handle.Init.Mode       = UART_MODE_TX_RX;
+	uart_handle.Init.OverSampling = UART_OVERSAMPLING_8;
+
+	if (HAL_UART_Init(&uart_handle) != HAL_OK)
+	{
+		printf("UART Init failed\n");
+		while (1);
+	}
+	printf("Uart enabled\n");
+	for(;;)
+	{
+		BSP_LED_Toggle(LED_GREEN);
+		BSP_LED_Toggle(LED_RED);
+		BSP_LED_Toggle(LED_ORANGE);
+		BSP_LED_Toggle(LED_BLUE);
+		for (int j = 0; j < 1000000; j++);
+		printf("Toggle\n");
+		HAL_UART_Transmit(&uart_handle, (uint8_t *)&ch, 1, 0xFFFF);
+	}
+	return 0;
 }
